c8072598734d430846b5db6e863f3f9196ef8220
Lorenzo Arcioni <57827177+lorenzo-arcioni@users.noreply.github.com>
2023-05-04 09:39:56 +0200

    Delete icons directory

--- icons/Python GUI/ui_to_py.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-python3 -m PyQt5.uic.pyuic -x main_GUI.ui -o main_GUI.py
-python3 -m PyQt5.uic.pyuic -x DB_insert_GUI.ui -o DB_insert_GUI.py
-python3 -m PyQt5.uic.pyuic -x main_splash_GUI.ui -o main_splash_GUI.py
-pyrcc5 resources.qrc -o resources_rc.py
-echo Fatto!
